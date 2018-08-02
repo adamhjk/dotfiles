aa812c93a9b5996019c48c26082915d349327571
Adam Jacob <adam@stalecoffee.org>
2018-08-01 21:15:35 -0700

    Send it!

--- install.sh
+++ install.sh
@@ -3,7 +3,15 @@
 set -e
 
 pacman -Syu
-pacman -S git fzf ripgrep zsh keychain vim
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+pacman -Sy git fzf ripgrep zsh keychain vim
+if [[ ! -d "$HOME/.zplug" ]]; then
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
+if [[ ! -d "$HOME/.homesick" ]]; then
+  git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+fi
 source ~/.homesick/repos/homeshick/homeshick.sh
 
+if [[ ! -d "$HOME/.homesick/repos/dotfiles" ]]; then
+  homeshick -b clone http://github.com/adamhjk/dotfiles
+fi
